75416d1754b4d1284f7f7ee7fbbfbbc31d9c2518
dws-nishoo <nishoo.gupta@rankwatch.com>
2021-02-17 18:00:49 +0530

    saved

--- employee-crud/src/routes/EmployeeRouter.ts
+++ employee-crud/src/routes/EmployeeRouter.ts
@@ -1,6 +1,9 @@
 import { Request, Response, Router, NextFunction, response } from 'express';
 import {EmployeeService} from "../services/employee.service";
 const {EmployeeNativeService} = require("../services/employee.native.services");
+import { ValidatedRequest, createValidator } from 'express-joi-validation';
+import { EmployeeValidations} from '../schema-validation/employee-validation';
+const validator = createValidator({ passError: true });
 class EmployeeRouter {
     public router: Router;
     constructor() {
@@ -9,7 +12,7 @@ class EmployeeRouter {
     }
     private mountRoutes() {
         this.router.get("/", this.getEmployee);
-        this.router.post("/add", this.addEmployee);
+        this.router.post("/add", [validator.body(EmployeeValidations.EmployeeSchema)], this.addEmployee);
         this.router.post("/update/:employeeId", this.updateEmployee);
         this.router.get("/delete/:employeeId", this.deleteEmployee);
         this.router.get("/native", this.getNativeEmployee);
@@ -38,6 +41,7 @@ class EmployeeRouter {
         return res.status(200).json(result);
     }
     async addEmployee(req: Request, res: Response, next: NextFunction) {
+        console.log("addEmployee");
         let data = req.body;
         let result = await EmployeeService.addEmployeeService(data);
         // return res.status(HttpStatusCodes.ok).json(result);
